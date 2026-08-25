aca65b8fa2e511cfacea9616f6a718f7ff09548d
JustCallMeJon <jveith30@gmail.com>
2026-08-25 09:18:23 -0600

    Corrected Product Relationship
    One category can now have many different products. Now, when you delete a product, it won't remove the entire category.

--- src/main/java/com/jtspringproject/JtSpringProject/models/Product.java
+++ src/main/java/com/jtspringproject/JtSpringProject/models/Product.java
@@ -21,7 +21,7 @@ public class Product {
 	
 	private String image;
 	
-	@OneToOne(cascade = CascadeType.ALL)
+	@ManyToOne
 	@JoinColumn(name = "category_id",referencedColumnName = "category_id")
 	private Category category;
 	
